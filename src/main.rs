c0f0fbcc70f2a8fe0ab0fc3718e30d504c0ad33f
storyfeet <thebeaky@psmail.net>
2019-02-25 22:00:20 +0000

    added clap to program so can ask to search for rules,  Needs more available rules added to the matches

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 mod pesto;
 use crate::pesto::Rule;
+use clap::{clap_app, crate_version};
+
 use pest::Parser;
 fn main() {
-    println!("Hello, world!");
+    let cp = clap_app!(pegtest =>
+    (version : crate_version!())
+    (about : "Tests how pegs work")
+    (author : "Matthew Stoodley,Zen3Ger")
+    (@arg rule: -r +takes_value "The rule to look for -- default Statement")
+    )
+    .get_matches();
+
+    let rule = match cp.value_of("rule").unwrap_or("Statement") {
+        "Range" => Rule::Range,
+        _ => Rule::Statement,
+    };
+
+    println!("Rule = {:?}", rule);
 
     loop {
         let mut s = String::new();
         std::io::stdin().read_line(&mut s).ok();
-        let r = pesto::Command::parse(Rule::Statement, &s).expect("Parse fail");
+        let r = pesto::Command::parse(rule, &s).expect("Parse fail");
         println!("res = {:?}", r);
     }
 }
