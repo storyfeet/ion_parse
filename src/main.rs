5f4780457ba9209f34deab67d2844f1a61979536
storyfeet <thebeaky@psmail.net>
2019-02-26 22:00:00 +0000

    Added some tests

--- src/main.rs
+++ src/main.rs
@@ -26,3 +26,56 @@ fn main() {
         println!("res = {:?}", r);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn should_pass() {
+        let v = vec![
+            (Rule::Statement, "let x = 4"),
+            (Rule::Statement, "for x in [0..4]; echo $x; end;"),
+            (Rule::Statement, "for x in [0..4]\n echo $x\n end;"),
+            (Rule::Range, "[0..4]"),
+            (Rule::Range, "$(ls -l)"),
+        ];
+
+        let mut errs = Vec::new();
+        for (n, (rl, st)) in v.iter().enumerate() {
+            if let Err(e) = pesto::Command::parse(*rl, &st) {
+                errs.push((n, rl, st, e));
+            }
+        }
+
+        if errs.len() > 0 {
+            for e in errs {
+                println!("{:?}\n", e);
+            }
+            panic!();
+        }
+    }
+    #[test]
+    fn should_fail() {
+        let v = vec![
+            (Rule::Statement, "let x & 4"),
+            (Rule::Statement, "for x in ls -l; echo $x; end;"),
+            (Rule::Statement, "for x in [0..4]\n echo $x\n end;"),
+            (Rule::Range, "[0..Green]"),
+            (Rule::Range, "$(ls -l)"),
+        ];
+
+        let mut errs = Vec::new();
+        for (n, (rl, st)) in v.iter().enumerate() {
+            if let Ok(v) = pesto::Command::parse(*rl, &st) {
+                errs.push((n, rl, st, v));
+            }
+        }
+
+        if errs.len() > 0 {
+            for e in errs {
+                println!("{:?}\n", e);
+            }
+            panic!();
+        }
+    }
+}
